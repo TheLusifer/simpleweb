4282644bdd038baee4914e7a9ce8fff8297fc0b2
lusifer <lusifer@localhost.localdomain>
2022-04-27 20:13:42 -0600

    aver si asi

--- app.py
+++ app.py
@@ -53,7 +53,4 @@ def link_submit():
 
 
 
-if os.environ.get('APP_LOCATION') == 'heroku':
-    run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
-else:
-    run(host='localhost', port=8080, reloader=True)
+run(host='localhost', port=8080, reloader=True)
